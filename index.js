f368ea203cba66159f1f7bffae20d95e9360e6fb
Tiffany Cheng <tiffanycheng.001@gmail.com>
2022-09-30 17:57:04 -0600

    commit tree e758a0854f1a9644396a91161bebfe1beed5cca6

---
+++ index.js
@@ -0,0 +1,39 @@
+  function add() {
+    // we'll fill this in shortly
+  }
+  function subtract() {
+    // we'll fill this in shortly
+  }
+  function multiply() {
+    // we'll fill this in shortly
+  }
+  function divide() {
+  // we'll fill this in shortly
+  }
+  function add(a,b) {
+    return a+b;
+  }
+  function subtract(a,b) {
+    return a-b;
+  }
+  function multiply(a,b) {
+    return a*b;
+  }
+  function divide(a,b) {
+    return a/b;
+  }
+
+  function increment(n){
+    return ++n
+  }
+  function decrement(n){
+    return --n
+  }
+  
+  function makeInt(n){
+    return parseInt(n,10)
+  }
+  function preserveDecimal(n){
+    return parseFloat(n)
+  }
+ 
